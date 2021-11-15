0360825d2f8e54e7ef659080cc9d39ff27321624
Robert Poenaru <robert.poenaru@protonmail.ch>
2021-11-15 20:45:01 +0200

    runs the deriv function

--- Physics/hamilton-equations/test.py
+++ Physics/hamilton-equations/test.py
@@ -17,7 +17,7 @@ def w3(n, y):
 x_values = np.arange(-1, 1, 0.01)
 y_values = []
 
-set_n = 1
+set_n = 0.1
 
 for x in x_values:
     try:
@@ -30,13 +30,18 @@ for x in x_values:
 # make arrays ok
 x_values = x_values[len(y_values):]
 
+print(len(x_values), len(y_values))
+
 # for id in range(len(x_values)):
 #     print(x_values[id], y_values[id])
 
 dx_values = [abs(x_values[id]) - abs(x_values[id - 1])
              for id in range(1, len(x_values))]
 
+# https://stackoverflow.com/a/26042315/8295213
 dy_values = np.gradient(y_values, np.mean(dx_values))
+
+print(len(dx_values))
 
 with open('w3.dat', 'w+') as file:
     for dx in dx_values:
@@ -46,25 +51,26 @@ with open('w3.dat', 'w+') as file:
 # dydx_values = np.gradient(y_values, dx_values)
 
 
-# def plot_data(xdata, ydata, file):
-#     plt.plot(xdata, ydata, label=f'n={set_n}')
-#     plt.xlabel('y')
-#     plt.ylabel(r'$w_3(n;y)$')
-#     plt.legend(loc='best')
-#     plt.savefig(file, bbox_inches='tight')
-#     plt.close()
+def plot_data(xdata, ydata, file):
+    plt.plot(xdata, ydata, label=f'n={set_n}')
+    plt.xlabel('y')
+    plt.ylabel(r'$w_3(n;y)$')
+    plt.legend(loc='best')
+    plt.savefig(file, bbox_inches='tight')
+    plt.close()
 
 
-# def plot_f_df(xdata, ydata, dydata, file):
-#     plt.plot(xdata, ydata, '-r', label=f'f')
-#     plt.plot(xdata, dydata, '-ok', label=f'f\'')
-#     plt.xlabel('y')
-#     plt.ylabel('fcts')
-#     plt.legend(loc='best')
-#     plt.savefig(file, bbox_inches='tight')
-#     plt.close()
+def plot_f_df(xdata, ydata, dydata, file):
+    # plt.plot(xdata, ydata, '-r', label=f'f')
+    plt.plot(xdata, dydata, '-ok', label=f'f\'')
+    plt.xlabel('y')
+    plt.ylabel('fcts')
+    plt.legend(loc='best')
+    plt.savefig(file, bbox_inches='tight')
+    plt.close()
 
 
-# # plot_data(x_values, y_values, 'w3.pdf')
-# # plot_data(np.arange(len(dx_values)), dx_values, 'w3_dx.pdf')
-# plot_f_df(x_values, y_values, np.mean(dx_values), 'fcts_comparison.pdf')
+# plot_data(x_values, y_values, 'w3.pdf')
+# plot_data(np.arange(len(dx_values)), dx_values, 'w3_dx.pdf')
+plot_f_df(np.array(x_values[1:]), np.array(y_values[1:]),
+          dx_values, 'fcts_comparison.pdf')
