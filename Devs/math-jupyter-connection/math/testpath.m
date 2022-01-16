f06c14f68a6a195414fcb4dd5535db49f6a6a83e
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-01-16 20:23:27 +0200

    find replacement function for NotebookDirectory call

--- Devs/math-jupyter-connection/math/testpath.m
+++ Devs/math-jupyter-connection/math/testpath.m
@@ -2,14 +2,15 @@
 
 (* ::Input:: *)
 ClearAll["Global`*"]
-
-
 data=Table[{x,2*x+1},{x,0,10}];
-
-Print[data]
-
 staticpath="/Users/robertpoenaru/Library/Mobile\ Documents/com~apple~CloudDocs/Work/Pipeline/DevWorkspace/Github/mathematica-useful-algorithms/Devs/math-jupyter-connection/math";
-
 finalpath=StringJoin[staticpath,"/data.csv"];
+Export[finalpath,TableForm[data]];
 
-Export[finalpath,TableForm[data]]
+
+(* get the current path of the file using a function that works within the script executables*)
+(* source: https://mathematica.stackexchange.com/questions/133276/how-to-find-source-of-frontendobjectnotavail-warning *)
+parentPath = $InputFileName;
+
+cleanpath=StringDrop[parentPath,-11];
+Print[cleanpath]
