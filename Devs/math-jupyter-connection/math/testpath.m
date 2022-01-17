0e3be42b8ca617a69e4496901549e164e2f4eb8f
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-01-17 14:09:20 +0200

    implement procedure for sbs joiner - notebook

--- Devs/math-jupyter-connection/math/testpath.m
+++ Devs/math-jupyter-connection/math/testpath.m
@@ -20,8 +20,14 @@ createTables[n_]:=Table[Table[{x,2*x+RandomReal[{1,10}]},{x,-limit,limit}],{i,1,
 (* function that joins two tables side-by-side *)
 lateralJoiner[table1_,table2_]:=Join[table1,table2,2];
 
+
+
 T4=createTables[4];
 t1=T4[[1]];
 t2=T4[[2]];
-Print[t1]
-Print[t2]
+(* Print[TableForm[t1]]
+Print[t2] *)
+
+tt={};
+tt=Do[lateralJoiner[t1,T4[[idx]]],{idx,2,Length[T4]}]
+Print[tt]
