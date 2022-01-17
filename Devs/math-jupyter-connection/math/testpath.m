7b1d892100d0beb93c461b36320307919aef6d22
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-01-17 08:21:41 +0200

    uses the test tables from the T4 object

--- Devs/math-jupyter-connection/math/testpath.m
+++ Devs/math-jupyter-connection/math/testpath.m
@@ -10,14 +10,18 @@ dataPath=StringJoin[StringDrop[parentPath,-15],"data/"];
 filename[name_,idx_,type_]:=StringJoin[dataPath,StringTemplate["``_``.``"][name,idx,type]];
 (* Export[filename["circle",1,".pdf"],Graphics[Circle[]]] *)
 
-(* test the joining process of two tables
+(* numerical parameters that will be used throughout the calculations *)
 limit=5;
 dx=0.1;
-t1=Table[{x,2*x+1},{x,-limit,limit}];
-t2=Table[{x,2*x-1},{x,-limit,limit}];
+
+(* test the joining process of two tables *)
 createTables[n_]:=Table[Table[{x,2*x+RandomReal[{1,10}]},{x,-limit,limit}],{i,1,n}];
+
 (* function that joins two tables side-by-side *)
 lateralJoiner[table1_,table2_]:=Join[table1,table2,2];
 
 T4=createTables[4];
-Print[T4] *)
+t1=T4[[1]];
+t2=T4[[2]];
+Print[t1]
+Print[t2]
