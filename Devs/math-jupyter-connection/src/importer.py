1845c57472a851ea0d4fec5ee4370b80da2aaa42
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-01-20 15:17:46 +0200

    update importer and gitignore

--- Devs/math-jupyter-connection/src/importer.py
+++ Devs/math-jupyter-connection/src/importer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+
 from matplotlib import pyplot as plt
 import numpy as np
 import os
@@ -6,8 +7,16 @@ import os
 # path to the /data directory
 datapath=str(os.getcwd())[:-4]+"/data/"
 
+#get the csv file from the /data directory
+csvfiles=[x for x in os.listdir(datapath) if ".csv" in x]
+
+
 # create the importer function
 def importCSV(csv_file):
-    with open(csv_file,'r+') as csv_reader:
+    with open(str(datapath)+str(csv_file),'r+') as csv_reader:
         firstline=csv_reader.readline()
+
     return firstline
+
+nocommas=importCSV(csvfiles[0])
+nocommas[0:3]
