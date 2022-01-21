109ee3c8b91daac3d82267be5265d223bf5b4339
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-01-21 08:49:59 +0200

    improves the `getParams` functio

--- Devs/math-jupyter-connection/src/importer.py
+++ Devs/math-jupyter-connection/src/importer.py
@@ -5,30 +5,44 @@ import numpy as np
 import os
 import math
 
+
 # path to the /data directory
 datapath = str(os.getcwd())[:-4] + "/data/"
+
 
 # get the csv file from the /data directory
 csvfiles = [x for x in os.listdir(datapath) if ".csv" in x]
 csv1 = csvfiles[0]
 
+# check if the number is odd
+
+
+def isodd(number):
+    try:
+        assert number % 2 != 0
+    except AssertionError as err:
+        return 0
+    return 1
+
+
 # create a function that reads the first line of the csv file and returns the number of parameter sets within the file
-
-
 def getParams(csv_file):
-    """ 
+    """
     - the function reads the first line of the csv file
     - the first line is stored in memory, then based on the number of commas, total number of sets is determined
     """
+
     with open(str(datapath) + str(csv_file), 'r+') as csv_reader:
         firstline = csv_reader.readline()
 
-    Nparams = (len([fl for fl in firstline if fl == ","]) + 1) / 2
-    print(Nparams%10)
-    return int(Nparams)
+    # perform a check for correct number of commas
+    Ncommas = len([fl for fl in firstline if fl == ","])
+    if(isodd(Ncommas) == 0):
+        return -1
+    else:
+        Nparams = int((Ncommas + 1) / 2)
 
+    return Nparams
 
-Nparams = getParams(csv1)
-print(f'There are {Nparams} parameter sets')
 
 # create a function that gets the values for all the parameter sets in the csv file
