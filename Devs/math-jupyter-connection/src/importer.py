28f230db7ed3f42c8c2264001a4f19ade67c0bc7
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-01-22 11:43:56 +0200

    add class docstring

--- Devs/math-jupyter-connection/src/importer.py
+++ Devs/math-jupyter-connection/src/importer.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 
 class CSVImporter:
+    """
+    - Import the numerical data inside the given CSV file
+    - The CSV file is represented by a string (path to the actual file) and it is given as a class argument when first initializing the class itself
+    - First the data is imported in raw-format
+    - Additional function for parsing the raw data into proper format that can be furthermore graphically represented is also implemented
+    """
+
     def __init__(self, datadirpath):
         self.datadirpath = datadirpath
 
