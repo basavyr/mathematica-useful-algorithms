64110a847afae1da9fa54162ef31d19d3935f070
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-01-22 13:14:15 +0200

    add class methods for importing the number of parameters

--- Devs/math-jupyter-connection/src/app.py
+++ Devs/math-jupyter-connection/src/app.py
@@ -6,12 +6,20 @@ import os
 # constants and relevant parameters/arguments that will be used throughout execution
 DATADIRNAME = "data/"
 DATADIRPATH = str(os.getcwd())[0:-3] + DATADIRNAME
+CSVFILES = [file for file in os.listdir(DATADIRPATH) if ".csv" in file]
+CSV_1 = CSVFILES[0]
+
+# lambda for appending the data directory path to the csf file name
+csvfilepath = lambda filename: f'{DATADIRPATH}{filename}'
 
 
 # This is the main (executable) app..
 def main():
-    file = csv.CSVImporter(DATADIRPATH)
-    file.showDirPath()
+    file = csv.CSVImporter(csvfilepath(CSV_1))
+    n_params = file.get_param_number()
+    # print(n_params)
+    param_values = file.get_param_values()
+    print(param_values)
 
 
 if __name__ == "__main__":
--- Devs/math-jupyter-connection/src/importer.py
+++ Devs/math-jupyter-connection/src/importer.py
@@ -8,11 +8,46 @@ class CSVImporter:
     - Additional function for parsing the raw data into proper format that can be furthermore graphically represented is also implemented
     """
 
-    def __init__(self, datadirpath):
-        self.datadirpath = datadirpath
+    def __init__(self, csv_file_path):
+        """at init, the class receives the path to the csv file
+        numerical data will be imported from that path
+        """
+        self.csv_file_path = csv_file_path
 
-    def showDirPath(self):
-        print(self.datadirpath)
+    def show_csv_path(self):
+        print(self.csv_file_path)
+
+    def get_param_number(self,):
+        with open(self.csv_file_path, 'r+') as reader:
+            first_line = reader.readline()
+        commas = [l for l in first_line if l == ","]
+        param_set_number = int((len(commas) + 1) / 2)
+
+        return param_set_number
+
+    def get_param_values(self):
+        raw_params = []
+
+        with open(self.csv_file_path, 'r+') as reader:
+            for idx in range(2):
+                current_line = next(reader).strip()
+                if(idx == 1):
+                    raw_params.append(current_line)
+
+        # stop if the `raw_params` array has dimension != 1
+        try:
+            assert len(raw_params) == 1
+        except AssertionError as err:
+            return -1
+        finally:
+            pass
+
+        params = [int(p) for p in raw_params[0].split(',')]
+        param_pairs = [[params[id], params[id + 1]]
+                       for id in range(0, len(params) - 1, 2)]
+        
+
+        return param_pairs
 
 
 def main():
