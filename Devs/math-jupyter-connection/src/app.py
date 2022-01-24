38550d366f4a8e3bff6ce55f96f917f283a91b29
Robert Poenaru <robert.poenaru@protonmail.ch>
2022-01-24 09:10:04 +0200

    add condition for console print

--- Devs/math-jupyter-connection/src/app.py
+++ Devs/math-jupyter-connection/src/app.py
@@ -8,6 +8,9 @@ DATADIRNAME = "data/"
 DATADIRPATH = str(os.getcwd())[0:-3] + DATADIRNAME
 CSVFILES = [file for file in os.listdir(DATADIRPATH) if ".csv" in file]
 CSV_1 = CSVFILES[0]
+# conditional for printing the data to console (1) or not (0)
+PRINT_TO_SCREEN = 0
+
 
 # lambda for appending the data directory path to the csf file name
 csvfilepath = lambda filename: f'{DATADIRPATH}{filename}'
@@ -19,7 +22,7 @@ def printer(main_cls, main_obj):
 
 
 # This is the main (executable) app...
-def main():
+def main(print_condition):
     file = csv.CSVImporter(csvfilepath(CSV_1))
     n_params = file.get_param_number()
     param_values = file.get_param_values()
@@ -27,10 +30,11 @@ def main():
     raw_data = file.get_raw_data()
     numerical_data = file.numerical_data()
 
-    # print the data to the console
-    obj_list = [n_params, param_values, legends, raw_data, numerical_data]
-    printer(file, obj_list)
+    if(print_condition == 1):
+        # print the data to the console
+        obj_list = [n_params, param_values, legends, raw_data, numerical_data]
+        printer(file, obj_list)
 
 
 if __name__ == "__main__":
-    main()
+    main(PRINT_TO_SCREEN)
