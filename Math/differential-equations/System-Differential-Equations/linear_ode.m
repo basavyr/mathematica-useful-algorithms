8235dd38975f6ea0b3afc889480652fa9e7945aa
Robert Poenaru <robert.poenaru@protonmail.ch>
2023-03-12 13:22:41 +0200

    add plot for differential equation

---
+++ Math/differential-equations/System-Differential-Equations/linear_ode.m
@@ -0,0 +1,17 @@
+(* solver for a linear ordinary differential equation *)
+
+diffEq = f'[t] == Power[t, 2] - f[t];
+
+sol = DSolve[diffEq, f[t], t]
+
+(* make the constant from the sol term as a variable *)
+
+solC[c_] :=
+    Values @ sol[[1, 1]] /. {C[1] -> c};
+
+Print["Solutions for f[t]"]
+
+Export["c_values.pdf", ListPlot[Table[{t, solC[1]}, {t, -1, 10}], Joined
+     -> True, PlotRange -> All, PlotStyle -> Red, PlotLabel -> "c=1", FrameLabel
+     -> {"t", "f[t]"}, Frame -> True, ImageSize -> 400, PlotMarkers -> {Automatic,
+     10}, Axes -> False, PlotRange -> All]]
